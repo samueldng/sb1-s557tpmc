31811e290494ada756fd16ad0833efade1e6dba6
Samuel Oliveira <63470561+samueldng@users.noreply.github.com>
2025-01-17 16:22:33 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://isqkhiplcgyzrlfrmkla.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
